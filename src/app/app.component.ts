acb9fecbcf2fb55cdd5f946b01804bdf48705b39
unknown <thibault.camuset@gmail.com>
2023-04-01 16:49:03 +0200

    [ADD] first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'hb_business_case_frontend';
+}
